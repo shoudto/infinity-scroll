fbfb4316d520409dcb13ee02f9fb7538822ab82b
Oscar Shoudt <oscarshoudt@Oscars-MBP.home>
2023-04-27 21:40:05 -0400

    setting up photos from api using displayPhotos() method

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
+// Dom Manipulation
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
 // Unsplash API
 const count = 10;
 const apiKey = 'SHeKmYp2G4A3Pjfvb3x-mQ28hdTcxSAhpOT52zDLjgg';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Create Elements for Links & Photos, Add to DOM
+function displayPhotos() {
+    // Run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        // Create <a> to link to Unsplash
+        const item = document.createElement('a');
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank');
+
+        // Create <img> for photo
+        const img = document.createElement('img');
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+        img.setAttribute('title', photo.alt_description);
+
+        // Put <img> inside <a>, then put both inside imageContainer
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+};
+
 // Get photos from unsplash api
 async function getPhotos() {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
     try {
-
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
     } catch (error) {
         // Catch Error Here!
         console.log('API not working!')
